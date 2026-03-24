69ef7b462b16c7c9fe8642821d542ad04e27378a
kiranbabup <kiran12146@gmail.com>
2026-03-25 00:38:04 +0530

    fb edited

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ const { initializeApp } = require("firebase/app");
 const { getStorage } = require("firebase/storage");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBd-Qx2NtwT4E3gTPO6G_gbolt6XCMAwT8",
-  authDomain: "tapxtream-64eea.firebaseapp.com",
-  projectId: "tapxtream-64eea",
-  storageBucket: "tapxtream-64eea.appspot.com",
-  messagingSenderId: "105148996870",
-  appId: "1:105148996870:web:bbade8f2068685586e1287"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
